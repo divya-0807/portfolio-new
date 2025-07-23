68dd8e1d4509116b51c873bc5bc9fb1e96e414f7
Divya <dlokwani8724@gmail.com>
2025-07-24 00:54:56 +0530

    YAYA hoggya

--- portfolio-3.0/src/data/ExperienceData.jsx
+++ portfolio-3.0/src/data/ExperienceData.jsx
@@ -3,7 +3,7 @@ export const ExperienceData = [
         timerange: "January 2025 - June 2025",
         company : "Department of Atomic Energy(DAE) RRCAT, Indore",
         role: "Software Trainee",
-        about: "Developed an attendance dashboard that automated PRIS report generation for over 1,500 employees, streamlining financial year reporting. Leveraged MongoDB aggregation pipelines to efficiently handle 3.2 million+ records across a decade, and significantly reduced data retrieval latency from 8 seconds to 1.5 seconds through optimized queries and indexing."
+        about: "Developed an attendance dashboard that automated PRIS report generation for over 1,500 employees, streamlining financial year reporting. Leveraged MongoDB aggregation pipelines to efficiently handle 3.2 million+ records across a decade, and significantly reduced data retrieval latency from 8 seconds to 1.5 seconds through optimized queries and indexing. Designed and implemented a modular microservice to automate the yearly claims workflow, seamlessly integrating approved reimbursements into the payroll system and eliminating manual processing for up to 2,000 claims annually. Enhanced PostgreSQL performance by reducing average query response time from 6 seconds to 2 seconds through indexing and query optimization. Additionally, digitized the claim submission process with a JSP-based UI, cutting manual effort by 40% and streamlining approvals for doctors, employees, and administrative staff."
     },
      {
         timerange: "June 2025 - Present",
