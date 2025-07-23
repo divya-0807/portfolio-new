0e670ba61bf143c832f74fb93574aa3d492707dd
Divya <dlokwani8724@gmail.com>
2025-07-24 00:51:01 +0530

    YAYA hoggya

--- portfolio-3.0/src/data/ExperienceData.jsx
+++ portfolio-3.0/src/data/ExperienceData.jsx
@@ -9,6 +9,6 @@ export const ExperienceData = [
         timerange: "June 2025 - Present",
         company : "Infosys",
         role: "Digital Specialist Engineer",
-        about: "Designed and implemented a modular microservice to automate the yearly claims workflow, seamlessly integrating approved reimbursements into the payroll system and eliminating manual processing for up to 2,000 claims annually. Enhanced PostgreSQL performance by reducing average query response time from 6 seconds to 2 seconds through indexing and query optimization. Additionally, digitized the claim submission process with a JSP-based UI, cutting manual effort by 40% and streamlining approvals for doctors, employees, and administrative staff."
+        about: "I'm currently undergoing intensive MERN stack training, focused on building production-ready applications for real-world use cases. Along the way, I've consistently secured Rank 1 in Konnect technical quizzes, standing out among 100+ peers. This experience is sharpening my skills in scalable development, clean architecture, and real-time problem solving."
     }
 ];
